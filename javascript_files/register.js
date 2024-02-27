89d4c4f4244df6581c0cf86d982cbefd68a4c667
Tyler Moessing <tmoessing@gmail.com>
2024-02-26 23:52:55 -0700

    worked on registartion page

--- javascript_files/register.js
+++ javascript_files/register.js
@@ -1,39 +1,53 @@
 'use strict';
 
 function register() {
+    let email = document.getElementById("email").value;
+    // if (!validateNewRegister(email)) {
+    //     document.getElementById("email").value = "";
+    //     document.getElementById("fname").value = "";
+    //     document.getElementById("lname").value = "";
+    //     document.getElementById("password").value = "";
+    //     document.getElementById("confirmPassword").value = "";
+
+    //     alert("Email already used to register");
+    //     return;
+    // }
+
     let fname  = document.getElementById("fname").value;
     let lname = document.getElementById("lname").value;
-    let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
     let confirmPassword = document.getElementById("confirmPassword").value;
 
+    // Validate that required fields are not empty
+    if (!fname || !lname || !email || !password || !confirmPassword) {
+        alert("Please fill out all required fields.");
+        return;
+    }
+
     let UserDataObject = createUserDataObject(fname, lname, email, password);
 
-    if (validatePassword(password, confirmPassword)){
-        let retrievdUserDataObject = JSON.parse(localStorage.getItem("userData"));
-        let newUserDataObject = retrievdUserDataObject;
-
-        newUserDataObject["total_users"] += 1;
-        let userID = newUserDataObject["total_users"];
-
-        Object.assign(newUserDataObject, {[userID]: UserDataObject});
-
-        localStorage.setItem("userData", JSON.stringify(newUserDataObject));
-
-        localStorage.setItem("currentUser", JSON.stringify(newUserDataObject));
-
-        window.location.href = "plan_event.html";   
-        // let retrievedUserDataObject = JSON.parse(localStorage.getItem("UserDataObject"));
-    } else {
-        // Clear all Boxes
-        document.getElementById("fname").value = "";
-        document.getElementById("lname").value = "";
-        document.getElementById("email").value = "";
+    if (!validatePassword(password, confirmPassword)){
+        // Clear Password Boxes
         document.getElementById("password").value = "";
         document.getElementById("confirmPassword").value = "";
 
         alert("Password's do not Match");
+        return;
     }
+
+        
+    let UserID = newUserID();
+
+    // Update Current Users
+    localStorage.setItem("currentUser", JSON.stringify({[UserID]: UserDataObject}));
+    // Appends to User Data
+    let retrievdUserDataObject = JSON.parse(localStorage.getItem("userData"));
+    let newUserDataObject = retrievdUserDataObject;
+    
+    Object.assign(newUserDataObject, {[UserID]: UserDataObject});
+    localStorage.setItem("userData", JSON.stringify(newUserDataObject));
+    window.location.href = "plan_event.html";
+
 }
 
 function validatePassword(password, confirmPassword) {
@@ -87,3 +101,30 @@ function createUserDataObject(fname, lname, email, password) {
 
     return UserDataObject;
 }
+
+function newUserID() {
+    let retrievdUserDataObject = JSON.parse(localStorage.getItem("userData"));
+    retrievdUserDataObject["total_users"] += 1;
+    localStorage.setItem("userData", JSON.stringify(retrievdUserDataObject));
+    return retrievdUserDataObject["total_users"];
+}
+
+// function validateNewRegister(email) {
+//     let retrievdUserDataObject = JSON.parse(localStorage.getItem("userData"));
+//     for (let userID in retrievdUserDataObject) {
+//         let userData = retrievdUserDataObject[userID]
+//         if (typeof UserData === 'object') {
+//             if (userData.hasOwnProperty('userinformation')){
+//                 let desiredUserDataTitle = userData.hasOwnProperty('userinformation')
+//                 if (desiredUserDataTitle.hasOwnProperty('email')) {
+//                     let emailstored = desiredUserDataTitle.hasOwnProperty('email')
+//                     if (emailstored === email) {
+//                         return false;
+//                     }
+//                 }
+//             }
+
+//         }
+//     }
+//     return true;
+// }
