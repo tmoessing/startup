102f211ff1bb4c9696a70b72dd095c4119ba0e20
Tyler Moessing <tmoessing@gmail.com>
2024-02-27 16:03:28 -0700

    finished working on javascript for log in page

--- javascript_files/log_in.js
+++ javascript_files/log_in.js
@@ -2,18 +2,22 @@ function logIn () {
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
 
-    let retrievdUserDataObject = JSON.parse(localStorage.getItem("userData"));
+    // Check if User Exists
+    let potenital_password = localStorage.getItem(email);
 
-    for (let user in retrievdUserDataObject){
-        if (user[userinformation]) {
-            if (user[userinformation][username] == email){
-                if (user[userinformation][username] == email){
-                    userID = user[userinformation][]
-            }
+    if (potenital_password !== null) {
+        if (potenital_password === password) {
+            localStorage.setItem("currentUser", email);
+
+            window.location.href = "hangout_hub.html";
+        } else {
+            alert("Incorrect Password");
+            document.getElementById("password").value = "";
         }
-
+    } else { 
+        alert("No user found. Please Create an Account")
+        window.location.href = "register.html";
     }
-
     
 
     // window.location.href = "plan_event.html";
