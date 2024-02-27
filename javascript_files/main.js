9646c4b6e81da76d2585e94b5dba8a558be87061
Tyler Moessing <tmoessing@gmail.com>
2024-02-27 16:08:12 -0700

    Had on page load create an eventData storage container

--- javascript_files/main.js
+++ javascript_files/main.js
@@ -6,13 +6,13 @@ let eventLocation
 let eventDescription
 
 function onPageLoad() {
-    let storedValue = localStorage.getItem("userData");
+    let storedValue = localStorage.getItem("eventData");
     if (storedValue !== null) {
         console.log("Database already created")
     } else {
         let userData = {total_users: 0};
         let eventData = {total_events: 0};
-        localStorage.setItem("userData", JSON.stringify(userData));
+        // localStorage.setItem("userData", JSON.stringify(userData));
         localStorage.setItem("eventData", JSON.stringify(eventData));
     }
     }
