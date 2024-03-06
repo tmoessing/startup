2187faf3c24cb2fc51988f16b4c76371e6b00f3c
Tyler Moessing <tmoessing@gmail.com>
2024-03-06 13:34:23 -0700

    have app load up the public folder

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('*', (req, res) => {
-  res.send('<h1>Hello Express!</h1>');
-});
+// JSON body parsing using built-in middleware
+app.use(express.json());
+
+// Serve up the front-end static content hosting
+app.use(express.static('public'));
 
 app.listen(3000);
