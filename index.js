5d8f1a427e02d31bec416f1e9726286b86ecf833
Tyler Moessing <tmoessing@gmail.com>
2024-03-22 12:14:06 -0600

    split index.js to have a secure api Rouger and regular

--- index.js
+++ index.js
@@ -1,48 +1,37 @@
-const mongoDB = require('./mongoDB.js');
 const bcrypt = require('bcrypt');
 const cookieParser = require('cookie-parser');
-
-
-
-// Create an HTTP service using Node.js and Express
 const express = require('express');
 const app = express();
 
-// The service port. In production the front-end code is statically hosted by the service on the same port.
-const port = process.argv.length > 2 ? process.argv[2] : 3000;
+const DB = require('./mongoDB.js');
 
-app.use(cookieParser());
+const authCookieName = 'token';
+
+// The service port may be set on the command line
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
 
-// Serve up the front-end static content hosting
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
+
+// Serve up the applications static content
 app.use(express.static('public'));
 
+// Trust headers that are forwarded from the proxy so we can determine IP addresses
+app.set('trust proxy', true);
 
-// Your backend provides service endpoints
+// Router for service endpoints
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
 
-// Get Events
-app.get('/pull-events', async (req, res) => {
-	console.log("HELLO?")
-	let event_list = await mongoDB.pullEvents();
-
-    res.send(event_list);
-});
-
-// Create Event
-app.post('/create-event', (req, res) => {
-    updateEventList(req.body);
-
-    res.send(200);
-    });
-
-// Authentication
-app.post('/auth/create', async (req, res) => {
-	if (await mongoDB.getUser(req.body.email)) {
+// CreateAuth token for a new user
+apiRouter.post('/auth/create', async (req, res) => {
+	if (await DB.getUser(req.body.email)) {
 		res.status(409).send({ msg: 'An account is already associated with this email' });
 	} else {
-		const user = await mongoDB.createUser(req.body.email, req.body.password);
+		const user = await dB.createUser(req.body.email, req.body.password);
 
 		setAuthCookie(res, user.token);
 
@@ -52,8 +41,9 @@ app.post('/auth/create', async (req, res) => {
 	}
 });
 
-app.post('/auth/login', async (req, res) => {
-	const user = await mongoDB.getUser(req.body.email);
+// GetAuth token for the provided credentials
+apiRouter.post('/auth/login', async (req, res) => {
+	const user = await DB.getUser(req.body.email);
 	if (user) {
 		if (await bcrypt.compare(req.body.password, user.password)) {
 			setAuthCookie(res, user.token);
@@ -64,37 +54,76 @@ app.post('/auth/login', async (req, res) => {
 	res.status(401).send({msg: 'Invalid Credentials'});
 });
 
-function setAuthCookie(res, authToken) {
-	res.cookie('token', authToken, {
-		secure: true,
-		httpOnly: true,
-		sameSite: 'strict',
-	});
-}
-
-app.get('/user/me', async (req, res) => {
-	authToken = req.cookies['token'];
-	const user = await mongoDB.connect_to_UserInformation().findOne({token: authToken});
-	if (user) {
-		res.send({ email: user.email });
-		return;
-	}
-	res.status(401).send({msg: 'Unauthorized'})
+// DeleteAuth token if stored in cookie
+apiRouter.delete('/auth/logout', (_req, res) => {
+  res.clearCookie(authCookieName);
+  res.status(204).end();
 });
 
-//Frontend served up using Express static middleware
+// GetUser returns information about a user
+apiRouter.get('/user/:email', async (req, res) => {
+  const user = await DB.getUser(req.params.email);
+  if (user) {
+    const token = req?.cookies.token;
+    res.send({ email: user.email, authenticated: token === user.token });
+    return;
+  }
+  res.status(404).send({ msg: 'Unknown' });
+});
 
-// Return application default page if the path is unknown
+// secureApiRouter verifies credentials for endpoints
+var secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+  authToken = req.cookies[authCookieName];
+  const user = await DB.getUserByToken(authToken);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+});
+
+// Get Events
+secureApiRouter.get('/pull-events', async (req, res) => {
+	let event_list = await DB.pullEvents();
+
+    res.send(event_list);
+});
+
+// Create Event
+secureApiRouter.post('/create-event', (req, res) => {
+    updateEventList(req.body);
+
+    res.send(200);
+    });
+
+// Default error handler
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
+});
+
+// Return the application's default page if the path is unknown
 app.use((_req, res) => {
-	res.sendFile('index.html', { root: 'public' });
+  res.sendFile('index.html', { root: 'public' });
+});
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
   });
-  
+}
+
 app.listen(port, () => {
-	console.log(`Listening on port ${port}`);
-  });
+  console.log(`Listening on port ${port}`);
+});
 
 // Update Events
 function updateEventList(event) {
-	mongoDB.createEvent(event);
+	DB.createEvent(event);
 
 }
---
+++ oldindex.js
@@ -0,0 +1,104 @@
+const mongoDB = require('./mongoDB.js');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+
+const authCookieName = 'token';
+
+// Create an HTTP service using Node.js and Express
+const express = require('express');
+const app = express();
+
+// The service port. In production the front-end code is statically hosted by the service on the same port.
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+app.use(cookieParser());
+
+// JSON body parsing using built-in middleware
+app.use(express.json());
+
+// Serve up the front-end static content hosting
+app.use(express.static('public'));
+
+
+// Your backend provides service endpoints
+
+// Get Events
+app.get('/pull-events', async (req, res) => {
+	let event_list = await mongoDB.pullEvents();
+
+    res.send(event_list);
+});
+
+// Create Event
+app.post('/create-event', (req, res) => {
+    updateEventList(req.body);
+
+    res.send(200);
+    });
+
+// Authentication
+app.post('/auth/create', async (req, res) => {
+	if (await mongoDB.getUser(req.body.email)) {
+		res.status(409).send({ msg: 'An account is already associated with this email' });
+	} else {
+		const user = await mongoDB.createUser(req.body.email, req.body.password);
+
+		setAuthCookie(res, user.token);
+
+		res.send({
+			id: user._id,
+		});
+	}
+});
+
+app.post('/auth/login', async (req, res) => {
+	const user = await mongoDB.getUser(req.body.email);
+	if (user) {
+		if (await bcrypt.compare(req.body.password, user.password)) {
+			setAuthCookie(res, user.token);
+			res.send({id:user._id});
+			return;
+		}
+	}
+	res.status(401).send({msg: 'Invalid Credentials'});
+});
+
+apiRouter.delete('/auth/logut', (_req, res) => {
+	res.clearCookie(authCookieName)
+	res.status(204).end();
+});
+
+function setAuthCookie(res, authToken) {
+	res.cookie('token', authToken, {
+		secure: true,
+		httpOnly: true,
+		sameSite: 'strict',
+	});
+}
+
+app.get('/user/me', async (req, res) => {
+	authToken = req.cookies['token'];
+	const user = await mongoDB.connect_to_UserInformation().findOne({token: authToken});
+	if (user) {
+		res.send({ email: user.email });
+		return;
+	}
+	res.status(401).send({msg: 'Unauthorized'})
+});
+
+//Frontend served up using Express static middleware
+
+// Return application default page if the path is unknown
+app.use((_req, res) => {
+	res.sendFile('index.html', { root: 'public' });
+  });
+  
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`);
+  });
+
+// Update Events
+function updateEventList(event) {
+	mongoDB.createEvent(event);
+
+}
