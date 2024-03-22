527ce41fba5d3e810f89e12015078a94e715b502
Tyler Moessing <tmoessing@gmail.com>
2024-03-22 12:02:03 -0600

    deleted local storage

--- public/javascript_files/main.js
+++ public/javascript_files/main.js
@@ -5,18 +5,6 @@ let eventTime
 let eventLocation
 let eventDescription
 
-function onPageLoad() {
-    let storedValue = localStorage.getItem("eventData");
-    if (storedValue !== null) {
-        console.log("Database already created")
-    } else {
-        let userData = {total_users: 0};
-        let eventData = [];
-        // localStorage.setItem("userData", JSON.stringify(userData));
-        localStorage.setItem("eventData", JSON.stringify(eventData));
-    }
-    }
-
 function displayQuote(data) {
     fetch('https://api.quotable.io/random')
       .then((response) => response.json())
