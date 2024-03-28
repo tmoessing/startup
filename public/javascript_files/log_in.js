fc850e53c5767591f2e4f04ce3c0c2405145a1a2
Tyler Moessing <tmoessing@gmail.com>
2024-03-28 11:14:19 -0600

    username is now stored in local storage when logged in

--- public/javascript_files/log_in.js
+++ public/javascript_files/log_in.js
@@ -11,14 +11,22 @@ function logIn () {
 }
 
 
-async function db_user_login(users_creditnals) {
+async function db_user_login(users_credentials) {
     const response = await fetch('/api/auth/login', {
             method: 'POST',
             headers: {'content-type': 'application/json'},
-            body: JSON.stringify(users_creditnals),
+            body: JSON.stringify(users_credentials),
     });
 
     if (response.ok) {
+        console.log(users_credentials.email);
+        const user = await fetch(`/api/user/${users_credentials.email}`, {
+            method: 'GET',
+            headers: {'content-type': 'application/json'},
+        })
+        userinfo = await user.json();
+
+        localStorage.setItem('userName', userinfo.username);
         window.location.href = "plan_event.html";
 
     } else {
