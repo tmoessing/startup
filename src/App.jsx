af3736a3bb09288be116be6fdb40d6477a1826ab
Tyler Moessing <tmoessing@gmail.com>
2024-04-06 13:32:14 -0600

    add footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 
+import Header from './header/header.jsx'
+import Footer from './footer/footer.jsx'
+
+import './index.css'
+import './main.css'
+
 function App() {
-    return(
+    return (
         <>
-            <p>Hello World</p>
+            <Header />
+            <nav class="yellow-nav-bar">
+                <menu>
+                    <a href="html_files/hangout_hub.html">Hang Out Hub</a>
+                    <a href="html_files/register.html">Register</a>
+                    <a href="html_files/log_in.html">Log In</a>
+                    <a href="..\index.html">Log Out</a>
+                    <a href="html_files/plan_event.html">Plan Event</a>
+                </menu>
+            </nav>
+
+            <Footer />
         </>
     )
 }
---
+++ src/footer/footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <>
+            <footer>
+                <h8>About the Author: Tyler Moessing</h8>
+                <div class="footer-links">
+                    <a href="https://github.com/tmoessing/startup" target="_blank">GitHub</a>
+                    <a href="https://www.linkedin.com/in/tyler-moessing" target="_blank">LinkedIn</a>
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
