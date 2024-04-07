abdf942602d1d789233fe8178c3ccc87b3c69f48
Tyler Moessing <tmoessing@gmail.com>
2024-04-06 21:48:04 -0600

    correct the h8 tag to a h6

--- src/footer/footer.jsx
+++ src/footer/footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <>
             <footer>
-                <h8>About the Author: Tyler Moessing</h8>
+                <h6>About the Author: Tyler Moessing</h6>
                 <div class="footer-links">
                     <a href="https://github.com/tmoessing/startup" target="_blank">GitHub</a>
                     <a href="https://www.linkedin.com/in/tyler-moessing" target="_blank">LinkedIn</a>
