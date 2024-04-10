ef8eb3b2984d058d0d664658a151e204e5212200
Tyler Moessing <tmoessing@gmail.com>
2024-04-09 21:05:37 -0600

    fixed class to be className

--- src/footer/footer.jsx
+++ src/footer/footer.jsx
@@ -4,8 +4,8 @@ function Footer() {
     return (
         <>
             <footer>
-                <h6>About the Author: Tyler Moessing</h6>
-                <div class="footer-links">
+                <h4>About the Author: Tyler Moessing</h4>
+                <div className="footer-links">
                     <a href="https://github.com/tmoessing/startup" target="_blank">GitHub</a>
                     <a href="https://www.linkedin.com/in/tyler-moessing" target="_blank">LinkedIn</a>
                 </div>
