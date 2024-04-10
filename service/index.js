3dce5d55aa0b5d2b34b1214e59871ce09b4073b5
Tyler Moessing <tmoessing@gmail.com>
2024-04-09 21:05:53 -0600

    changed port so vite could work

--- service/index.js
+++ service/index.js
@@ -9,7 +9,7 @@ const { peerProxy } = require('./peerProxy.js');
 const authCookieName = 'token';
 
 // The service port may be set on the command line
-const port = process.argv.length > 2 ? process.argv[2] : 4000;
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
