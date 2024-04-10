0bf5f9627cfacc2615eeefe3031a95c2201c4112
Tyler Moessing <tmoessing@gmail.com>
2024-04-09 23:10:55 -0600

    basic outline for hangout hub

--- src/hangout_hub/hangout_hub.jsx
+++ src/hangout_hub/hangout_hub.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import './hangout_hub.css'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import loadEvents from './hangout_hub.js';
+
+import './hangout_hub.css';
 
 function HangoutHub() {
+    const navigate = useNavigate();
+
+    loadEvents(navigate);
+
     return (
         <>
-            Hello From Hangout Hub
+            <section className="hangout_hub_table">
+                <div id="title">
+                    <h2>Hangout Hub</h2>
+                </div>
+                <div id="table">
+                    <span className="loader"></span>
+                    <table>
+
+                    </table>
+                </div>
+            </section>
         </>
     )
 }
