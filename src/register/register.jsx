6abdd585c0d462b452793d86057c206a9471d0a9
Tyler Moessing <tmoessing@gmail.com>
2024-04-09 22:30:47 -0600

    quote is now working so removed test stuff

--- src/register/register.jsx
+++ src/register/register.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
-
-import './register.css'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './register.css';
 
 function Register() {
+    const navigate = useNavigate();
+
+    function register() {
+        navigate('')
+    }
+
     return (
         <>
-            Hello From Register
+            <div className="register">
+                <h2>Register</h2>
+                <div>
+                    <div className="form-floating">
+                        <input type="text" className="form-control" id="username" name="username" placeholder="" required/>
+                            <label htmlFor="username">Username</label>
+                    </div>
+                    <div className="form-floating">
+                        <input type="email" className="form-control" id="email" name="email" placeholder="name@example.com" required/>
+                            <label htmlFor="email">Email Address</label>
+                    </div>
+                    <div className="form-floating">
+                        <input type="password" className="form-control" id="password" name="password" placeholder="Password" required/>
+                            <label htmlFor="password">Password</label>
+                    </div>
+                    <div className="form-floating">
+                        <input type="password" className="form-control" id="confirmPassword" name="confirmPassword" placeholder="Password" required/>
+                            <label htmlFor="confirmPassword">Confirm Password</label>
+                    </div>
+                    <button className="btn btn-primary w-100 py-2" onclick={ register }>Register</button>
+                </div>
+            </div>
         </>
     )
 }
